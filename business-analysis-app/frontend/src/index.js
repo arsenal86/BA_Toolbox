c4862924246aefd1d28fc600fa82840b7c0d9a5b
arsenal86 <113337307+arsenal86@users.noreply.github.com>
2025-06-19 16:23:51 +0100

    Update index.js

--- business-analysis-app/frontend/src/index.js
+++ business-analysis-app/frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Note the change here
+import './index.css';
 import App from './App';
 
-ReactDOM.render(
+// 1. Create a root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 2. Render your app into the root
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
